2925a596796315a330a506eb252247cb514b813a
zhangshun <zhangshun@growingio.com>
2023-05-28 22:09:31 +0800

    feat: 增加浮点数的处理

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,22 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
+        double d = 2.6;
+        int n = (int)(d + 5);
+        System.out.println("n = " +  n);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double a = 1.0;
+        double b = 3.0;
+        double c = -4.0;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        double r1 = 0;
+        double r2 = 0;
+
+        double e = b * b - 4 * a * c;
+
+       r1 = (-b + Math.sqrt(e)) / (2 * a);
+       r2 =  (-b - Math.sqrt(e)) / (2 * a);
+       System.out.println(r1 == 1 && r2 == -4 ? "测试通过" : "测试失败");
     }
 }
